59ae5ba303c5858fe6a963fe975a5cef005d2606
Cao Thiện Thiên Tân <alxarrow123@gmail.com>
2023-09-03 22:01:36 +0700

    Create entity for categories, sizes, admin function

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useParams, useRouter } from 'next/navigation';
+
+import { CategoryColumn, columns } from './columns';
+
+import { Plus } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Heading } from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import { ApiList } from '@/components/ui/api-list';
+import { DataTable } from '@/components/ui/data-table';
+
+interface CategoryClientProps {
+  data: CategoryColumn[];
+}
+export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className='flex items-center justify-between'>
+        <Heading
+          title={`Categories (${data.length})`}
+          description='Manage categories for your store'
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <Plus className='mr-2 h-4 w-4' />
+          Add new
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey='name' columns={columns} data={data} />
+      <Heading title='API' description='API calls for Categories' />
+      <Separator />
+      <ApiList entityName='categories' entityIdName='categoryId'/>
+    </>
+  );
+};
