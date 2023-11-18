8e063f6da8c209f8c691b209e732d21b091da8f6
Cao Thiện Thiên Tân <alxarrow123@gmail.com>
2023-11-18 23:21:40 +0700

    Overview Admin Dashboard

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,15 +1,26 @@
+import { formatter } from '@/lib/utils';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+import { CreditCard, DollarSign, Package } from 'lucide-react';
+
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
-import prismadb from '@/lib/prismadb';
-import { formatter } from '@/lib/utils';
-import { CreditCard, DollarSign, Package } from 'lucide-react';
+import { Overview } from '@/components/overview';
+
+import { getTotalRevenue } from '@/actions/get-total-revenue';
+import { getSalesCount } from '@/actions/get-sales-count';
+import { getStockCount } from '@/actions/get-stock-count';
+import { getGraphRevenue } from '@/actions/get-graph-revenue';
 
 interface DashboardPageProps {
   params: { storeId: string };
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
+  const graphRevenue = await getGraphRevenue(params.storeId);
   return (
     <div className='flex-col'>
       <div className='flex-1 space-y-4 p-8 pt-6'>
@@ -24,7 +35,9 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
               <DollarSign className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent>
-              <div className='text-3xl font-bold'>{formatter.format(100)}</div>
+              <div className='text-3xl font-bold'>
+                {formatter.format(totalRevenue)}
+              </div>
             </CardContent>
           </Card>
           <Card>
@@ -33,19 +46,29 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
               <CreditCard className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent>
-              <div className='text-3xl font-bold'>+25</div>
+              <div className='text-3xl font-bold'>+{salesCount}</div>
             </CardContent>
           </Card>
           <Card>
             <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-              <CardTitle className='text-sm font-medium'>Products In Stock</CardTitle>
+              <CardTitle className='text-sm font-medium'>
+                Products In Stock
+              </CardTitle>
               <Package className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent>
-              <div className='text-3xl font-bold'>12</div>
+              <div className='text-3xl font-bold'>{stockCount}</div>
             </CardContent>
           </Card>
         </div>
+        <Card className='col-span-4'>
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className='pl-2 '>
+            <Overview data={graphRevenue} />
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
